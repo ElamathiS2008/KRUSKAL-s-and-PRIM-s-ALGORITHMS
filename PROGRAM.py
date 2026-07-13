6b2e364b75c11838dcf5aca921794a31b10955d5
ElamathiS2008 <elamathii0202@gmail.com>
2026-07-13 22:03:55 +0530

    Update PROGRAM.py

--- PROGRAM.py
+++ PROGRAM.py
@@ -1,1 +1,81 @@
+import heapq
 
+# ---------- Union-Find for Kruskal ----------
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+
+        if rx == ry:
+            return False
+
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+
+        self.parent[ry] = rx
+
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+
+        return True
+
+
+# ---------- Kruskal Algorithm ----------
+def kruskal(n, edges):
+    edges.sort()
+
+    uf = UnionFind(n)
+    mst = []
+    cost = 0
+
+    for w, u, v in edges:
+        if uf.union(u, v):
+            mst.append((u, v, w))
+            cost += w
+
+            if len(mst) == n - 1:
+                break
+
+    return mst, cost
+
+
+# ---------- Prim Algorithm ----------
+def prim(n, adj, start=0):
+    key = [float('inf')] * n
+    parent = [-1] * n
+    inMST = [False] * n
+
+    key[start] = 0
+    pq = [(0, start)]
+
+    mst = []
+    cost = 0
+
+    while pq:
+        w, u = heapq.heappop(pq)
+
+        if inMST[u]:
+            continue
+
+        inMST[u] = True
+
+        if parent[u] != -1:
+            mst.append((parent[u], u, w))
+            cost += w
+
+        for v, wt in adj.get(u, []):
+            if not inMST[v] and wt < key[v]:
+                key[v] = wt
+                parent[v] = u
+                heapq.heappush(pq, (wt, v))
+
+    return mst, cost
